0e91687ed4a91910a339db368a36a1986562f4bc
David C. Rankin <drankinatty@gmail.com>
2017-02-24 03:25:38 -0600

    Removed commented code from gtk_menu.[ch] move.

--- gtk_windef.h
+++ gtk_windef.h
@@ -29,45 +29,6 @@ gboolean on_window_delete_event (GtkWidget *widget, GdkEvent *event,
                                  kwinst *app);
 void on_window_destroy (GtkWidget *widget, kwinst *app);
 
-/* menu callbacks */
-// void menu_file_new_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_file_open_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_file_reload_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_file_save_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_file_saveas_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_file_pagesu_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_file_pprev_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_file_print_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_file_close_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_file_quit_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_undo_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_redo_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_copy_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_cut_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_paste_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_delete_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_find_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_replace_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_goto_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_edit_preferences_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_font_select_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_status_clear_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_status_properties_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_status_bigredbtn_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_tools_indent_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_tools_unindent_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_tools_insfile_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_tools_toupper_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_tools_tolower_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_tools_totitle_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_tools_join_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_help_about_activate (GtkMenuItem *menuitem, kwinst *app);
-// #ifdef HAVESOURCEVIEW
-// void menu_view_linehl_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_view_lineno_activate (GtkMenuItem *menuitem, kwinst *app);
-// void menu_tools_syntax_activate (GtkMenuItem *menuitem, kwinst *app);
-// #endif
-
 /* misc. callback functions */
 gboolean on_keypress (GtkWidget *widget, GdkEventKey *event, kwinst *app);
 void on_insmode (GtkWidget *widget, kwinst *app);
